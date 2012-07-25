d152e7a46beda794a2842841b9c6ec9b0a0bf323
Gabor Cselle <gaborcselle@gmail.com>
2012-07-25 14:58:38 -0700

    Temporary fix for MailCore <-> reMail breakage

--- pull_dependencies.sh
+++ pull_dependencies.sh
@@ -12,6 +12,9 @@ git clone git://github.com/facebook/three20.git
 git clone git://github.com/mronge/MailCore.git
 cd mailcore
 git submodule update --init
+# TODO(gabor): Temporary fix - current version of MailCore doesn't work with reMail
+# see https://groups.google.com/d/msg/remail-iphone/SmnEeX9N6As/f-2iYzvAiGcJ
+git checkout 7c13b7b9518e0bc2cdd86e092245e0e05afa0239
 cd -
 
 # this should be it - you can now open the ReMailIPhone Xcode project
